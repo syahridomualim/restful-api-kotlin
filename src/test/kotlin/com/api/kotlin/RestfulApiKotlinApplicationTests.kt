7a0de1710b0defb7f174cb33bb91352821375add
Mu'alim Syahrido <54626091+syahridomualim@users.noreply.github.com>
2023-01-22 09:01:13 +0700

    Update RestfulApiKotlinApplicationTests.kt

--- src/test/kotlin/com/api/kotlin/RestfulApiKotlinApplicationTests.kt
+++ src/test/kotlin/com/api/kotlin/RestfulApiKotlinApplicationTests.kt
@@ -3,10 +3,10 @@ package com.api.kotlin
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+// @SpringBootTest
 class RestfulApiKotlinApplicationTests {
 
-	@Test
+// 	@Test
 	fun contextLoads() {
 	}
 
